0ed63f467225c81d0cc074bec3423ea37434fbcd
malan88 <m@stdwtr.io>
2020-07-18 13:12:03 -0400

    Get template more or less working

--- src/templates/poll.js
+++ src/templates/poll.js
@@ -4,12 +4,23 @@ import Img from 'gatsby-image'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import pollsters from "../images/national/pollster/pollsters.json"
+import states from "../images/states/states.json"
+
+
 export default ({ data }) => {
+  let title = ""
+  if (data.file.name in pollsters) {
+    title = pollsters[data.file.name];
+  } else if (data.file.name in states) {
+    title = states[data.file.name];
+  }
+
   return (
     <Layout>
       <SEO title="temp" />
       <div>
-        {data.file.name}
+        <h1>{title}</h1>
         <Img fluid={data.file.childImageSharp.fluid} />
       </div>
     </Layout>
@@ -21,7 +32,7 @@ export const query = graphql`
     file(fields: { slug: { eq: $slug } }) {
       name
       childImageSharp {
-        fluid {
+        fluid(quality: 100, maxWidth: 3000) {
           ...GatsbyImageSharpFluid
         }
       }
