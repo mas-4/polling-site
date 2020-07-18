f8ff9fef32097cd2f5b37fc1ebfe18cf2bc56b3b
malan88 <m@stdwtr.io>
2020-07-18 13:45:01 -0400

    Get all links going on index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { graphql, Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -6,13 +7,52 @@ import SEO from "../components/seo"
 import pollsters from "../images/national/pollster/pollsters.json"
 import states from "../images/states/states.json"
 
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
+  const pollster_links = [];
+  const state_links = [];
+
+  data.allFile.edges.forEach( ({node}) => {
+    if (node.name in pollsters) {
+      pollster_links.push({
+        name: node.name,
+        slug: node.fields.slug,
+        title: pollsters[node.name],
+      });
+    } else if (node.name in states) {
+      state_links.push({
+        name: node.name,
+        slug: node.fields.slug,
+        title: states[node.name],
+      });
+    }
+  });
+
   return (
     <Layout>
       <SEO title="Home" />
+      <h1>National Polling</h1>
       <ul>
-        {Object.keys(pollsters).map((key) => (
-          <li key={key}>{pollsters[key]}</li>
+        <li><Link to="/national/since_april">All Polls Since April</Link></li>
+        <li><Link to="/national/all">All Polls</Link></li>
+      </ul>
+      <h1>Individual National Pollsters</h1>
+      <ul>
+        {pollster_links.map((node) => (
+          <li key={node.name}>
+            <Link to={node.slug}>
+              {node.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <h1>State Polls</h1>
+      <ul>
+        {state_links.map((node) => (
+          <li key={node.name}>
+            <Link to={node.slug}>
+              {node.title}
+            </Link>
+          </li>
         ))}
       </ul>
     </Layout>
@@ -20,3 +60,17 @@ const IndexPage = () => {
 }
 
 export default IndexPage
+export const query = graphql`
+  query {
+    allFile {
+      edges {
+        node {
+          name
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  }
+`
