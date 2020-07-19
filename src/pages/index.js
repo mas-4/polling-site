08687fe7bb871a42b609ac04433092cb0fdcb9b1
malan88 <m@stdwtr.io>
2020-07-19 10:15:15 -0400

    SOrt state names

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,6 +27,18 @@ const IndexPage = ({ data }) => {
     }
   });
 
+  state_links.sort((a,b) => {
+    const nameA = a.name.toUpperCase();
+    const nameB = b.name.toUpperCase();
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+    return 0;
+  })
+
   return (
     <Layout>
       <SEO title="Home" />
