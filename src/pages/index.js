bab59bc4eebaf05d76a22ec0291bf955559d854c
malan88 <m@stdwtr.io>
2020-07-24 09:05:25 -0400

    Sort pollster names

--- src/pages/index.js
+++ src/pages/index.js
@@ -38,6 +38,17 @@ const IndexPage = ({ data }) => {
     }
     return 0;
   })
+  pollster_links.sort((a,b) => {
+    const nameA = a.name.toUpperCase();
+    const nameB = b.name.toUpperCase();
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+    return 0;
+  })
 
   return (
     <Layout>
